c21c7c5972744bd4b1f6ad0897099f6bec2ffbf4
Ryan Walden <waldenr1@gmail.com>
2021-09-21 22:25:31 -0400

    records fine, issue formating sample for model

--- app.py
+++ app.py
@@ -2,33 +2,68 @@ import torch
 import zipfile
 import torchaudio
 from glob import glob
-from flask import Flask, render_template, request, redirect
+import os
+import uuid
+from flask import Flask, flash, request, redirect
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'audio\\')
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    device = torch.device('cpu')  # gpu also works, but our models are fast enough for CPU
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
-    model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                        model='silero_stt',
-                                        language='en', # also available 'de', 'es'
-                                        device=device)
-    (read_batch, split_into_batches,
-    read_audio, prepare_model_input) = utils  # see function signature for details
 
-    # download a single file, any format compatible with TorchAudio (soundfile backend)
-    torch.hub.download_url_to_file('https://opus-codec.org/static/examples/samples/speech_orig.wav',
-                                dst ='speech_orig.wav', progress=True)
-    test_files = glob('speech_orig.wav')
+@app.route('/save-record', methods=['POST'])
+def save_record():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    
+    file = request.files['file']
+
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+
+    # save sample
+    file_name = str(uuid.uuid4()) + ".wav"
+    full_file_name = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    file.save(full_file_name)
+    
+    # format sample for model
+    test_files = glob(full_file_name)
+    app.logger.info(test_files)
     batches = split_into_batches(test_files, batch_size=10)
     input = prepare_model_input(read_batch(batches[0]),
                                 device=device)
+    
+    # voice to text prediction
     text = ''
     output = model(input)
     for example in output:
         text += decoder(example.cpu())
-    return text
+    app.logger.info(text)
+    return '<h1>Success</h1>'
+    
 
 if __name__ == "__main__":
+    # gpu also works, but our models are fast enough for CPU
+    device = torch.device('cpu')  
+
+    # languages also available 'de', 'es'
+    model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                                        model='silero_stt',
+                                        language='en', 
+                                        device=device)
+
+    # see function signature for details
+    (read_batch, split_into_batches,
+    read_audio, prepare_model_input) = utils  
+
     app.run(debug=True, threaded=True)
